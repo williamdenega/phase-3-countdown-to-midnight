f42784a97af9cafb0929c4592cdfe3ba65f22082
William Denega <williamdenega@gmail.com>
2023-10-27 11:42:14 -0400

    commit tree 5d712680ea2643d224d84d9572f6b9ff452fb60b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
-
-def countdown
+require 'pry'
+def countdown sec
+    #sec.times {|count| puts "#{count} SECOND(S)!"}
+    (1..sec).reverse_each { |count| puts "#{count} SECOND(S)!"}
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep sec
+    (1..sec).reverse_each do |count|
+        puts "#{count} SECOND(S)!"
+        sleep(1)
+    end
+end
+
+
+binding.pry
